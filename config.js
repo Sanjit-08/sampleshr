43b3743008eca8f2297e466eb405065f1595789c
Sanjit <kurarsanjit8@gmail.com>
2021-06-10 14:29:01 +0530

    Config file added

---
+++ config.js
@@ -0,0 +1,12 @@
+let allApi = {
+  HOME_URL: "https://repairadda.com:8443/shramin",
+  employer: "/employer",
+  candidate: "/candidate",
+  videoResume: "/candidate/videoResume",
+  signup: "/signup",
+  getEmployerData: "/employer",
+  master: "/master",
+  job: "/job",
+};
+
+export { allApi };
